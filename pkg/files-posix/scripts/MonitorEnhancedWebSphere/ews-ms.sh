222a4d81769eb3beb7ea29552b28e1869ba51947
Balaji Natarajan <balajin.nsfz@gmail.com>
2014-01-21 14:22:17 +0530

    Fixed mibdirs and php absolute path for the script withing files-posix directory. Repacked the upk.

--- pkg/files-posix/scripts/MonitorEnhancedWebSphere/ews-ms.sh
+++ pkg/files-posix/scripts/MonitorEnhancedWebSphere/ews-ms.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-../../apache/bin/php ews-ms.php $1
+inst=`grep ^inst /etc/init.d/uptime_httpd | cut -d= -f2`
+MIBDIRS=$inst/mibs
+export MIBDIRS
+
+/usr/local/uptime/apache/bin/php ews-ms.php $1
